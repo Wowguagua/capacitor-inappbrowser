f3c70ab7fbefd72f225f2389ec8de8d2e0091ba4
Wowguagua <huiyuan.tseng@justxtar.com>
2026-01-16 01:29:37 +0800

    feat(android): webview previous btn

--- android/src/main/java/com/wowguagua/plugin/inappbrower/InAppBrowserActivity.java
+++ android/src/main/java/com/wowguagua/plugin/inappbrower/InAppBrowserActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowInsets;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class InAppBrowserActivity extends Activity {
@@ -29,6 +29,7 @@ public class InAppBrowserActivity extends Activity {
         View headerContainer = findViewById(R.id.in_app_browser_header_container);
         TextView header = findViewById(R.id.in_app_browser_header);
         WebView webView = findViewById(R.id.in_app_browser_webview);
+        ImageButton backButton = findViewById(R.id.in_app_browser_back);
         View closeButton = findViewById(R.id.in_app_browser_close);
 
         Uri uri = Uri.parse(url);
@@ -37,6 +38,11 @@ public class InAppBrowserActivity extends Activity {
         String title = getIntent().getStringExtra(EXTRA_TITLE);
         header.setText(title != null ? title : host != null ? host : "In-App Browser");
 
+        backButton.setOnClickListener(v -> {
+            if (webView.canGoBack()) {
+                webView.goBack();
+            }
+        });
         closeButton.setOnClickListener(v -> finish());
 
         int headerBaseTopPadding = headerContainer.getPaddingTop();
@@ -55,7 +61,13 @@ public class InAppBrowserActivity extends Activity {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                updateBackButtonVisibility(view, backButton);
+            }
+        });
+        updateBackButtonVisibility(webView, backButton);
         webView.loadUrl(url);
     }
 
@@ -63,5 +75,9 @@ public class InAppBrowserActivity extends Activity {
     public void finish() {
         super.finish();
         overridePendingTransition(0, R.anim.slide_out_down);
+    }
+
+    private void updateBackButtonVisibility(WebView webView, View backButton) {
+        backButton.setVisibility(webView.canGoBack() ? View.VISIBLE : View.GONE);
     }
 }
